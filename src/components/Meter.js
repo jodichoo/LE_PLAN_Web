817f438e719dec3dce8aec64d7a25c16e4d4f683
Shee Hui <tansheehui@gmail.com>
2021-06-12 17:54:17 +0800

    Fix the meter

--- src/components/Meter.js
+++ src/components/Meter.js
@@ -1,8 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useAuth } from "../contexts/AuthContexts";
 import { db } from "../firebase";
-import Moment from "moment";
-import CustomProperties from "react-custom-properties";
 import DonutChart from "react-donut-chart";
 
 function Meter() {
@@ -11,8 +9,7 @@ function Meter() {
   const [workTime, setWorkTime] = useState(0);
   const [lifeTime, setLifeTime] = useState(0);
   const [totalTime, setTotalTime] = useState(0);
-  const [workRatio, setWorkRatio] = useState(0);
-  const [lifeRatio, setLifeRatio] = useState(0);
+  const [hovered, setHovered] = useState("work");
 
   useEffect(() => {
     userTasks.onSnapshot((doc) => {
@@ -21,54 +18,45 @@ function Meter() {
       const t = w + l;
       setLifeTime(l);
       setWorkTime(w);
-      setLifeTime(t);
-      setWorkRatio((workTime / totalTime) * 100 / 2);
-      setLifeRatio((lifeTime / totalTime) * 100 / 2);
+      setTotalTime(t);
     });
   }, []);
 
+  function enterMeter(item) {
+    return item.label === 'work' ? setHovered('Work') : setHovered('Play');
+  }
+
   return (
     <div>
+    <p>{hovered}</p>
       <DonutChart
         data={[
           {
             label: "work",
-            value: 20,
-            className: "work-meter"
+            value: workTime,
+            className: "workmeter"
           },
           {
             label: "life",
-            value: 30,
-            className: "life-meter"
+            value: lifeTime,
+            className: "meter"
           },
           {
               label:'',
-              value: 50,
+              value: totalTime,
           }
         ]}
         height={150}
         width={300}
         startAngle={180}
         legend={false}
-        colors={['#f5efef','rosybrown']}
+        colors={['#8a5858','#eddfc2']}
+        strokeColor={['#ffffff', '#ffffff']}
+        colorFunction={(colors, index) => colors[(index % colors.length)]}
+        formatValues={(values, total) => `${(values * 2 / total * 100).toFixed(1)}%`}
+        clickToggle={false}
+        onMouseEnter={(item) => {enterMeter(item)}}
       />
-      <CustomProperties
-        properties={{
-          "--first": "workRatio",
-          "--second": "lifeRatio",
-        }}
-      >
-        <div className="donut">
-          <div className="donut__slice donut__slice__first"></div>
-          <div className="donut__slice donut__slice__second"></div>
-          <div className="donut__label">
-            <div className="donut__label__heading">
-              Meter {workTime}/{lifeTime}
-            </div>
-            <div className="donut__label__sub">wOW aMaZInG!!</div>
-          </div>
-        </div>
-      </CustomProperties>
     </div>
   );
 }
