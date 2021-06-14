31e1a0c22f151765b2ae2905420f70f6c6448954
Shee Hui <tansheehui@gmail.com>
2021-06-14 20:29:40 +0800

    Add easter egg hehe

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,43 +1,48 @@
-import CenterDashboard from './CenterDashboard'; 
-import LeftDashboard from './LeftDashboard';
-import RightDashboard from './RightDashboard'; 
-import { useEffect, useState } from 'react'; 
-import { useHistory } from 'react-router-dom'; 
-import { useAuth } from '../contexts/AuthContexts'; 
-import { db } from '../firebase'; 
-
+import CenterDashboard from "./CenterDashboard";
+import LeftDashboard from "./LeftDashboard";
+import RightDashboard from "./RightDashboard";
+import { useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
+import { useAuth } from "../contexts/AuthContexts";
+import { db } from "../firebase";
+import ChromeDinoGame from "react-chrome-dino";
 
 function Dashboard() {
-    const currDate = new Date().toLocaleDateString('en-CA');
-    const [tasks, setTasks] = useState([]); 
-    const { currentUser } = useAuth(); 
-    const userTasks = db.collection('users').doc(currentUser.uid);
-    
-    useEffect(() => {
-        //get collection of tasks to be displayed 
-        const today = userTasks.collection(currDate); 
-        //order collection by time, then push each item in collection into array 
-        const unsubscribe = today.orderBy('time').onSnapshot((querySnapshot) => {
-            const t = []; 
-            querySnapshot.forEach(doc => {
-                t.push(doc.data());
-            })
-            //set local tasks variable to array t 
-            setTasks(t); 
-        })
-        return unsubscribe;
-    }, [])
+  const currDate = new Date().toLocaleDateString("en-CA");
+  const [tasks, setTasks] = useState([]);
+  const { currentUser } = useAuth();
+  const userTasks = db.collection("users").doc(currentUser.uid);
 
-    return (
-        <div className="dash">
-            {/* Left */}
-            <LeftDashboard /> 
-            {/* Center */}
-            <CenterDashboard tasks={tasks} setTasks={setTasks}/>
-            {/* Right */}
-            <RightDashboard tasks={tasks}/> 
-        </div>
-    )
+  useEffect(() => {
+    //get collection of tasks to be displayed
+    const today = userTasks.collection(currDate);
+    //order collection by time, then push each item in collection into array
+    const unsubscribe = today.orderBy("time").onSnapshot((querySnapshot) => {
+      const t = [];
+      querySnapshot.forEach((doc) => {
+        t.push(doc.data());
+      });
+      //set local tasks variable to array t
+      setTasks(t);
+    });
+    return unsubscribe;
+  }, []);
+
+  return (
+    <div classname="easter-egg">
+      <div className="dash">
+        {/* Left */}
+        <LeftDashboard />
+        {/* Center */}
+        <CenterDashboard tasks={tasks} setTasks={setTasks} />
+        {/* Right */}
+        <RightDashboard tasks={tasks} />
+      </div>
+      <div className="stevie-boy">
+        <ChromeDinoGame />
+      </div>
+    </div>
+  );
 }
 
-export default Dashboard; 
+export default Dashboard;
--- src/components/LeftDashboard.js
+++ src/components/LeftDashboard.js
@@ -1,9 +1,11 @@
 function LeftDashboard() {
-    return (
-        <div className="left-dash">
-            <h1>Calendar</h1>
-        </div>
-    )
+  return (
+    <div className="left-dash">
+      <div>
+        <h1>Calendar</h1>
+      </div>
+    </div>
+  );
 }
 
-export default LeftDashboard; 
+export default LeftDashboard;
