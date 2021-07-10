5f9aa255ec9c5ecdfb0ccaaa3159a8b075b9400b
Jodi Choo <jodi.choo@gmail.com>
2021-07-11 00:52:08 +0800

    Found way to check for valid image url

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -28,16 +28,30 @@ function Settings() {
 
     function handleSetProfilePic(e) {
         e.preventDefault(); 
-        currentUser.updateProfile({
-            photoURL: picUrl
-        })
-            .then(() => {
-                setSuccess('Successfully changed profile picture!');
-                setPicUrl('');
+        var image = new Image();
+        image.src = picUrl;
+
+        image.onload = function() {
+          if (this.width > 0) {
+            //the image exists, so update profile picture
+            currentUser.updateProfile({
+                photoURL: picUrl
             })
-            .catch(error => {
-                setError('Failed to set profile picture, please check the image url');
-            });
+                .then(() => {
+                    setError(''); 
+                    setSuccess('Successfully changed profile picture!');
+                    setPicUrl('');
+                })
+                .catch(error => {
+                    setError('Failed to set profile picture, please try again');
+                });
+          }
+        }
+
+        image.onerror = function() {
+          //image does not exist, set an error 
+          return setError('The image does not exist, please check the url');
+        }
     }
 
     function handleChangePassword(e) {
@@ -112,38 +126,52 @@ function Settings() {
         });
     }
 
-    const profilePicStyle = {
-        height: '200px', 
-        width: '250px', 
-        borderRadius: '100px'
-    }
 
     return (
         <div className='setting-profile-screen'>
             <div className='back' onClick={history.goBack}><IoChevronBackOutline style={{fontSize: '20px'}}/>Back</div>
+
             <div className='settings-container'>
                 <div>{success && <p>{success}</p>}</div>
-                <img style= {profilePicStyle} src={currentUser.photoURL} onError={(e)=>{e.target.onError = null; e.target.src="https://i.stack.imgur.com/l60Hf.png"}}/>
-                <h1>{currentUser.displayName}</h1>
-                <p>Upload Profile Picture</p>
-                    <form onSubmit={handleSetProfilePic}>
-                        <input type='text' value={picUrl} onChange={e => setPicUrl(e.target.value)} placeholder='e.g. pic.png, pic.jpg'required/>{' '}
-                        <button type='submit'>Set Picture</button>
-                    </form>
-                <p>Your Username: {username}</p>
-                <p>Your Email: {currentUser.email}</p>
-                <p className='toggle-reauth' onClick={() => setConfirmPassP(true)}>Change Password</p>
+                <div className='profile-pic'>
+                    <img style={{width: '100%', height: 'auto'}} src={currentUser.photoURL} onError={(e)=>{e.target.onError = null; e.target.src="https://i.stack.imgur.com/l60Hf.png"}}/>
+                </div>
+
+                <div className='display-name'>{currentUser.displayName}</div>
+
+                <div className='upload-pic'>
+                    Upload Profile Picture
+                    <div className='error'>{(error && !changePass) && <p>{error}</p>}</div>
+                        <form onSubmit={handleSetProfilePic}>
+                            <input type='text' value={picUrl} onChange={e => setPicUrl(e.target.value)} placeholder='e.g. pic.png, pic.jpg'required/>{' '}
+                            <button type='submit'>Set Picture</button>
+                        </form>
+                </div>
+                <div className='credentials'>
+                    <div className='profile-field'>
+                        <div className='label'>Your Username:</div>
+                        <div className='value'>{username}</div>
+                    </div>
+                    <div className='profile-field'>
+                        <div className='label'>Your Email:</div>
+                        <div className='value'>{currentUser.email}</div>
+                    </div>
+                </div>
+                
+                <div className='toggle-reauth' onClick={() => {setConfirmPassP(true); setError('')}}>Change Password</div>
                 <div className='error'>{(error && changePass) && <p>{error}</p>}</div>
                 {confirmPassP && toggleAuth()}
-                {changePass && <div>
-                    <form onSubmit={handleChangePassword}>
-                    New password:{' '}<input type='password' onChange={e => setNewPassword(e.target.value)} required></input>
-                    Confirm new password:{' '}<input type='password' onChange={e => setConfPassword(e.target.value)} required></input>
-                    <button type='submit'>Submit</button>
-                    </form>
-                    <button onClick={() => setChangePass(false)}>Cancel</button>
+                {changePass && <div className='settings-form'>
+                        <form onSubmit={handleChangePassword}>
+                            <div className='form-field'>New password:{' '}<input type='password' onChange={e => setNewPassword(e.target.value)} required></input></div>
+                            <div className='form-field'>Confirm new password:{' '}<input type='password' onChange={e => setConfPassword(e.target.value)} required></input></div>
+                            <div className='form-buttons'>
+                                <button type='submit'>Submit</button>{' '}
+                                <button onClick={() => {setChangePass(false); setError('')}}>Cancel</button>
+                            </div>
+                        </form>
                     </div>} 
-                <p className='toggle-reauth' onClick={() => setConfirmPassN(true)}>Change Display Name</p>
+                <div className='toggle-reauth' onClick={() => {setConfirmPassN(true); setError('')}}>Change Display Name</div>
                 {confirmPassN && toggleAuth()}
                 {changeName && <div>
                     <form onSubmit={handleChangeName}>
