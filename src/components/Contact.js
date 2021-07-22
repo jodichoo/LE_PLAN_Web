e2ddda47fb731ed8ad1a6a5d16add374713004f8
Jodi Choo <jodi.choo@gmail.com>
2021-07-22 18:54:29 +0800

    Made landing page sexc

---
+++ src/components/Contact.js
@@ -0,0 +1,19 @@
+function Contact(){
+    return (
+        <div className='contact'>
+            <p className='info'>
+                Singapore, Singapore, Singapore <br />
+                +65 6565 6565 <br />
+                baochickawaowao@gmail.com (this is a legit email)<br />
+            </p>
+
+            <p className='creators'>
+            <h3>CREATORS:</h3>
+                <div>Tan Shee Hui</div>
+                <div>Jodi Choo</div>
+            </p>
+        </div>
+    )
+} 
+
+export default Contact;
--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,24 +1,26 @@
 import { Link } from 'react-router-dom';
-
+import Logo from '../assets/logo.png';
+import { useState, useEffect } from 'react';
+import Contact from './Contact';
 
 function LandingPage() {
+    const [home, setHome] = useState(true);
+
     return (
         <div id='landing-page'> 
-                <nav className='nav-bar'>
-                    <ul>
-                        <li><Link to='/login' id="login-link" style={{textDecoration: 'none'}}>Login</Link></li>
-                        <li>Home</li>
-                    </ul>
-                </nav>
-            {/* Some other random text propaganda */}
-            <div id="apology">
-                <p>
-                    Hello World.<br />
-                    Sry homepage design is still a work in progress T_T
-                    Sry homepage design is still a work in progress T_T
-                    Sry homepage design is still a work in progress T_T
-                </p>
+            {home ? <div style={{height: "100px", marginTop: '20px'}}></div> : <img style={{width: 'auto', height: '100px', marginTop: '20px'}} src="https://cdn.discordapp.com/attachments/639487542432890900/867690623191351296/applogo.png" alt='oops'></img>}
+            <div className='nav-bar'>
+                <div className='nav-bar-element' onClick={() => setHome(true)}>HOME</div>
+                <div className='nav-bar-element'>
+                    <Link to='/login'>LOG IN</Link>
+                </div>
+                <div className='nav-bar-element' onClick={() => setHome(false)}>CONTACT</div>
             </div>
+            {home 
+            ? <><img className='home-logo' src="https://cdn.discordapp.com/attachments/639487542432890900/867690623191351296/applogo.png" alt='oops'></img>
+            <Link className='sign-up-button' to='/signup'>JOIN US</Link> </>
+
+            : <Contact />}
         </div>
     )
 }
