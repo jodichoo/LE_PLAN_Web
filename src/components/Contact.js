61d26d7ea3f20a883fa70617977d48fdc331e863
Shee Hui <tansheehui@gmail.com>
2021-07-25 02:10:47 +0800

    update contact page + landing page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,19 +1,31 @@
-function Contact(){
-    return (
-        <div className='contact'>
-            <p className='info'>
-                Singapore, Singapore, Singapore <br />
-                +65 6565 6565 <br />
-                baochickawaowao@gmail.com (this is a legit email)<br />
-            </p>
+function Contact() {
+  return (
+    <div className="contact">
+      <p className="info">
+        Singapore, Singapore, Singapore <br />
+        +65 6565 6565 <br />
+        baochickawaowao@gmail.com (this is a legit email)
+        <br />
+      </p>
 
-            <p className='creators'>
-            <h3>CREATORS:</h3>
-                <div>Tan Shee Hui</div>
-                <div>Jodi Choo</div>
-            </p>
+      <p className="creators">
+        <h3>CREATORS:</h3>
+        <div>Tan Shee Hui</div>
+        <div>Jodi Choo</div>
+      </p>
+      <p>
+        <h3>OUR GITHUB REPO:</h3>
+        <div>
+          Access LE PLAN Mobile App Repository{' '}
+          <a className="repo" href={"https://github.com/sheehui/LE_PLAN_APP"}>here</a>
         </div>
-    )
-} 
+        <div>
+          Access LE PLAN Web App Repository{' '}
+          <a className="repo" href={"https://github.com/sheehui/WLBBOT"}>here</a>
+        </div>
+      </p>
+    </div>
+  );
+}
 
 export default Contact;
--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,29 +1,59 @@
-import { Link } from 'react-router-dom';
-import { useState } from 'react';
-import Contact from './Contact';
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import Contact from "./Contact";
 
 function LandingPage() {
-    const [home, setHome] = useState(true);
+  const [home, setHome] = useState(true);
 
-    return (
-        <div id='landing-page'> 
-            {home 
-                ? <div className='logo-spacing'></div> 
-                : <img className='small-logo' src="https://cdn.discordapp.com/attachments/639487542432890900/867690623191351296/applogo.png" alt='oops'></img>}
-            <div className='nav-bar'>
-                <div className='nav-bar-element' style={{borderBottom: home ? '2px solid whitesmoke' : 'none'}} onClick={() => setHome(true)}>HOME</div>
-                <div className='nav-bar-element'>
-                    <Link to='/login'>LOG IN</Link>
-                </div>
-                <div className='nav-bar-element' style={{borderBottom: home ? 'none' : '2px solid whitesmoke'}} onClick={() => setHome(false)}>CONTACT</div>
-            </div>
-            {home 
-            ? <><img className='home-logo' src="https://cdn.discordapp.com/attachments/639487542432890900/867690623191351296/applogo.png" alt='oops'></img>
-            <Link className='sign-up-button' to='/signup'>JOIN US</Link> </>
-
-            : <Contact />}
+  return (
+    <div id="landing-page">
+      {home ? (
+        <div className="logo-spacing"></div>
+      ) : (
+        <img
+          className="small-logo"
+          src="https://cdn.discordapp.com/attachments/639487542432890900/867690623191351296/applogo.png"
+          alt="oops"
+        ></img>
+      )}
+      <div className="nav-bar">
+        <div
+          className="nav-bar-element"
+          style={{ borderBottom: home ? "2px solid whitesmoke" : "none" }}
+          onClick={() => setHome(true)}
+        >
+          HOME
         </div>
-    )
+        <div className="nav-bar-element">
+          <Link to="/login">LOG IN</Link>
+        </div>
+        <div
+          className="nav-bar-element"
+          style={{ borderBottom: home ? "none" : "2px solid whitesmoke" }}
+          onClick={() => setHome(false)}
+        >
+          CONTACT
+        </div>
+      </div>
+      {home ? (
+        <>
+          <img
+            className="home-logo"
+            src="https://cdn.discordapp.com/attachments/639487542432890900/867690623191351296/applogo.png"
+            alt="oops"
+          ></img>
+          <p className="phrase">
+              START YOUR JOURNEY TO WORK LIFE BALANCE WITH US
+          </p>
+          <Link className="sign-up-button" to="/signup">
+            JOIN US
+          </Link>{" "}
+        </>
+      ) : (
+        <Contact />
+      )}
+    </div>
+  );
 }
 
-export default LandingPage; 
+export default LandingPage;
